059bb3bed3c42db4e2d9b261fea5a566e7b5f652
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-08-29 09:28:46 -0400

    navbar/footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,8 +1,18 @@
 import './footer.scss';
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      {/* <img src={Logo} alt="" /> */}
+      <Link className="link" to="/">
+        {/* <img src={Logo} alt="" /> */}
+        <h2> the KAB [Block] </h2>
+      </Link>
+      <span>
+      <span>© .KAB International Ltd. 2023</span>
+      </span>
+  </footer>
   )
 }
 
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,53 @@
 import './navbar.scss';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+    <div className="container">
+      <div className="logo">
+        <Link className="link" to="/">
+        {/* <img src={Logo} alt="" /> */}
+        <h2> the KAB [Block] </h2>
+        </Link>
+      </div>
+      <div className="links">
+        <Link className="link" to="/?cat=realestate">
+          <h6>REAL ESTATE</h6>
+        </Link>
+        <Link className="link" to="/?cat=economics">
+          <h6>ECONOMICS</h6>
+        </Link>
+        <Link className="link" to="/?cat=technology">
+          <h6>TECHNOLOGY</h6>
+        </Link>
+        <Link className="link" to="/?cat=culture">
+          <h6>CULTURE</h6>
+        </Link>
+        <Link className="link" to="/?cat=design">
+          <h6>DESIGN</h6>
+        </Link>
+        <Link className="link" to="/?cat=food">
+          <h6>FOOD</h6>
+        </Link>
+        <span>USERNAME</span>
+        {/* <span>{currentUser?.username}</span>
+        {currentUser ? (
+          <span onClick={logout}>Logout</span> */}       
+          <span>LOGOUT</span>
+        {/* ) : ( */}
+          <Link className="link" to="/login">
+            Login
+          </Link>
+        {/* )} */}
+        <span className="write">
+          <Link className="link" to="/write">
+            Write
+          </Link>
+        </span>
+      </div>
+    </div>
+  </div>
   )
 }
 
