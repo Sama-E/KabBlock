308783668a5b8e02c3e9e11a2c5e0a26d47287b9
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-08-28 22:30:31 -0400

    update

--- client/src/pages/auths/login/Login.jsx
+++ client/src/pages/auths/login/Login.jsx
@@ -1,8 +1,33 @@
 import './login.scss';
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="auth">
+      <form>
+      <h1>Login</h1>
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          // onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          // onChange={handleChange}
+        />
+        {/* <button onClick={handleSubmit}>Login</button> */}
+        <button>Login</button>
+        {/* {err && <p>{err}</p>} */}
+        <span>
+          Don't you have an account? <Link to="/register">Register</Link>
+        </span>
+      </form>
+    </div>
   )
 }
 
--- client/src/pages/auths/register/Register.jsx
+++ client/src/pages/auths/register/Register.jsx
@@ -1,8 +1,40 @@
 import './register.scss';
+import { Link, useNavigate } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="auth">
+    <form>
+    <h1>Register</h1>
+      <input
+        required
+        type="text"
+        placeholder="username"
+        name="username"
+        // onChange={handleChange}
+      />
+      <input
+        required
+        type="email"
+        placeholder="email"
+        name="email"
+        // onChange={handleChange}
+      />
+      <input
+        required
+        type="password"
+        placeholder="password"
+        name="password"
+        // onChange={handleChange}
+      />
+      {/* <button onClick={handleSubmit}>Register</button> */}
+      <button>Register</button>
+      {/* {err && <p>{err}</p>} */}
+      <span>
+        Do you have an account? <Link to="/login">Login</Link>
+      </span>
+    </form>
+  </div>
   )
 }
 
