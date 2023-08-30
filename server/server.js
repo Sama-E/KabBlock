fc60544fc2d56bb91b2618d866b3f7f98b0ef03c
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-08-30 07:55:06 -0400

    cors update

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./services/auth/routes/auths.js";
 import blogRoutes from "./services/blog/routes/posts.js";
 
 const app = express();
 
+//Middlewares
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(express.json());
+app.use(cors({origin: "http://localhost:5173" }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
